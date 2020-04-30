0d29768ae591920cab51d8fb5f1b129f533340dc
Jim Wang <jimcwx@gmail.com>
2020-04-30 13:32:16 -0400

    change port

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/api/data', (req, res) => {
 
 
 // Start the application
-const listener = app.listen(process.env.PORT || 3389, () => {
+const listener = app.listen(process.env.PORT || 80, () => {
   // get the port from the listener.
   const port = listener.address().port;
 
